8f9f1d86bb6ae09dda23c8d2207a2321e8fa4efe
i401 <i401@uauth.io>
2022-11-12 18:01:33 +0100

    fix path travesal vulnerability

--- src/handlers/security.rs
+++ src/handlers/security.rs
@@ -10,8 +10,14 @@ pub async fn pgp_key() -> Result<String, status::ErrorHandler> {
 }
 
 pub async fn well_known(Path(file): Path<String>) -> Result<String, status::ErrorHandler> {
-    match &fs::read(format!("/etc/artemis/well-known/{}", file)) {
-        Ok(f) => Ok(String::from_utf8_lossy(f).parse().unwrap()),
-        Err(_) => Err(status::code_generic(500).await),
-    }
+	// TODO 
+	// need to fix this :D
+	if file.as_str() == "security.txt" {
+		match &fs::read(format!("/etc/artemis/well-known/{}", file)) {
+			Ok(f) => Ok(String::from_utf8_lossy(f).parse().unwrap()),
+			Err(_) => Err(status::code_generic(500).await),
+		}
+	} else {
+		Err(status::code_generic(418).await)
+	}
 }
